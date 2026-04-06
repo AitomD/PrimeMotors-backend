25930a5a116e8387faa9b1d1ac5f6717fa799626
FernandoConsolinRosa <128821668+FernandoConsolinRosa11@users.noreply.github.com>
2026-04-06 15:58:29 -0300

    IniciandoTestesUnitáriosService

---
+++ tests/unit/createUserService.test.ts
@@ -0,0 +1,25 @@
+import { createUserService } from '../../src/services/userService';
+import prisma from '../../src/config/database';
+import bcrypt from 'bcrypt';
+
+jest.mock('../../src/config/database', () => ({
+  user: {
+    findUnique: jest.fn(),
+    create: jest.fn(),
+  },
+}));
+jest.mock('bcrypt');
+
+describe('UserService - createUserService', () => {
+  it('deve lançar erro se o e-mail já estiver em uso', async () => {
+    const userData = { email: 'existente@teste.com', password: '123' };
+
+    (prisma.user.findUnique as jest.Mock).mockResolvedValue({ id: '1', ...userData });
+
+    await expect(createUserService(userData))
+      .rejects
+      .toThrow('E-mail já cadastrado!');
+    
+    expect(prisma.user.create).not.toHaveBeenCalled();
+  });
+});
