3c51310ffeff9e1702236faa1fa6df23414ea530
FernandoConsolinRosa <128821668+FernandoConsolinRosa11@users.noreply.github.com>
2026-04-08 15:12:21 -0300

    Garagem BackInit

---
+++ src/routes/userGarageRoutes.ts
@@ -0,0 +1,7 @@
+import { Router } from 'express';
+import { toggleFavorite} from '../controllers/favoriteController'
+import { authMiddleware } from '../middlewares/authMiddleware';
+
+const router = Router();
+
+router.get('/favorites/:userId', authMiddleware ,toggleFavorite);
